b533954886c3fca31588e3d8b010aa3fa8dffbf3
Luis Nava <luiseduardo@Felipes-MacBook-Pro.local>
2023-05-05 15:58:05 -0600

    v1

---
+++ src/routes/persons.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const router=express.Router();
+const mongoose=require('mongoose');
+let Person=require('../models/persons');
+
+router.get('/gente',async (req,res)=>{
+    let data = await Person.find({}); 
+    res.render('index', {data})
+});
+
+router.post('/updatePerson',async (req,res)=>{
+    Person.findByIdAndUpdate(req.body.objId,{
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss
+    })
+    .then((data)=>{res.redirect('/gente')})
+    .catch((error=>{res.json({message: error})}))
+});
+module.exports=router;
